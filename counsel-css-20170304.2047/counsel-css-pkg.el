3c4be5b8ffdc0611e5f41c58a5fa9e0feaf76c5e
showgood <showgood21@gmail.com>
2018-01-13 21:51:40 -0500

    add ivy

---
+++ counsel-css-20170304.2047/counsel-css-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "counsel-css" "20170304.2047" "stylesheet-selector-aware swiper" '((counsel "0.7.0") (cl-lib "0.5")) :authors '(("Henrik Lissner <http://github/hlissner>")) :maintainer '("Henrik Lissner" . "henrik@lissner.net") :url "https://github.com/hlissner/emacs-counsel-css")
